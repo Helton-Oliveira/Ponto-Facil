b756d6b9e2f6554bd78fa6bd605adfb2c0af76ef
Helton-Oliveira <heltonhenriqueoliveira321@gmail.com>
2026-06-10 17:17:08 -0300

    initial commit

---
+++ apps/backend/src/main/java/com/digisphere/pontofacil/DemoApplication.java
@@ -0,0 +1,13 @@
+package com.digisphere.pontofacil;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
